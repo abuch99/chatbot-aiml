d3e208fc55746453760a1748fa44dcdcea279a3a
arnav <abuch99@gmail.com>
2020-03-24 18:47:28 +0530

    Working Chatbot API

--- app.py
+++ app.py
@@ -24,29 +24,23 @@ kernel=aiml.Kernel()
 kernel.learn("bot/start.aiml")
 kernel.respond("learn ai")
 
-app = Flask(__name__)
-@app.route("/ask", methods=['POST','GET'])
-def ask():
-    message = str(request.args['chatmessage'])
-    resp=kernel.respond(message)
-
-    if str(message)[:6]=="google" or str(message)[:6]=="Google":
-	    url = "https://www.google.com.tr/search?q={}".format(str(message)[6:])
+while True:
+    u=input(">>>")
+    s=kernel.respond(u)
+    if str(u)[:6]=="google" or str(u)[:6]=="Google":
+	    url = "https://www.google.com.tr/search?q={}".format(str(u)[6:])
 	    webbrowser.open(url,2)
-	    insert(str(message),"")
-
+	    insert(str(u),"")
+	    continue
     else :
-        resp=resp.split('newline')
+        s=s.split('newline')
         ch=False
         v=""
-        for item in resp:
-            return jsonify({"status":"ok", "answer":str(item)})
+        for item in s:
+            print(str(item))
             v=v+str(item)
             if 'Bye' in item:
 	            ch=True
-        insert(message,resp)
+        insert(u,v)
         if ch :
-            pass
-
-if __name__ == "__main__":
-    app.run(debug=True)
+            break
